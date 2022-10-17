9881460b67567ac6525a204561b77ffeff80890a
00komiya <114364060+00komiya@users.noreply.github.com>
2022-10-17 13:09:06 +0000

    [finish]4-21

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,12 @@ devise_for :users
 root to: 'homes#top'
 
 resources :post_images, only: [:new, :create, :index, :show, :destroy]
+resources :users, only: [:show, :edit, :update]
+
+resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+ resource :favorites, only: [:create, :destroy]
+ resources :post_comments, only: [:create, :destroy]
+end
 
 get "/homes/about" => "homes#about", as: "about"
 
