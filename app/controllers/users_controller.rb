beaf81344ee28c29cb7805f469dbd3a99a5a7523
00komiya <114364060+00komiya@users.noreply.github.com>
2022-10-18 10:43:07 +0000

    [finish]4-24

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:page])  
@@ -10,9 +12,9 @@ class UsersController < ApplicationController
   
   
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to user_path(user.id)  
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)  
   end
   
   private
@@ -20,4 +22,15 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image)
   end
+
+  # ここから追加
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    login_user_id = current_user.id
+    if(user_id != login_user_id)
+      redirect_to post_images_path
+    end
+  end
+  # ここまで追加
+  
 end
